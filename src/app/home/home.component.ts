3d71aa4350c4b875bb687fd9be507f0cb22195e5
Jake <harrisonjake4@gmail.com>
2022-09-21 16:32:49 -0400

    002 | Added time functionality to Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  time = new Date();
+  intervalId;
 
-  constructor() { }
+  ngOnInit() {
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
 
-  ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
