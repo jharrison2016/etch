bb54c82a31bea975ae135c49be0ab3631e0e80dc
Jake <harrisonjake4@gmail.com>
2022-09-23 11:40:16 -0400

    010 | Adds functionality to calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,10 +7,40 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  
+  values: string[] = [];
+  length: number = 0;
+  command: string = "";
+  sum: number = 0;
+  counter: number = 12;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  insertVal(val) {
+    switch(val){
+      case '=':
+        this.command = "";
+        for(let v in this.values){
+          this.command += this.values[v];
+          console.log(this.values[v]);
+        }
+        this.sum = eval(this.command);
+        this.values=[];
+        this.values= this.sum.toString().split('');
+        this.length = this.values.length;
+        break;
+      
+      case 'c':
+        this.values = [];
+        break;
+      default:
+        this.values.push(val);
+        this.length = this.values.length;
+        break;
+    }
   }
 
 }
